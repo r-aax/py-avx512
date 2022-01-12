847e1653d35a882099f13f5fa9c4e2a20e43e0e7
Alexey Rybakov <rybakov.aax@gmail.com>
2022-01-12 14:23:30 +0300

    #1 : print cfg

---
+++ pyavx512/cfg/__init__.py
@@ -0,0 +1,4 @@
+from cfg.edge import Edge
+from cfg.graph import Graph
+from cfg.node import Node
+from cfg.parser import Parser
--- pyavx512/test.py
+++ pyavx512/test.py
@@ -2,57 +2,48 @@
 Test file.
 """
 
-from cfg.graph import Graph
-from cfg.node import Node
-from cfg.edge import Edge
-from cfg.parser import Parser
-from sem.oper import Oper, OperType
+import cfg
+import du
 
 # ==================================================================================================
 
 
 def case_001_build_manual():
     """
-    Build case_001 manually.
+    Build case_001 inner representation manually.
     """
 
-    cfg = Graph()
+    g = cfg.Graph()
 
     # Node 0.
-    node0 = cfg.new_node()
-    node0.Opers = []
+    n0 = g.new_node()
+    n0.Opers = []
 
     # Node 1.
-    node1 = cfg.new_node()
-    node1.Opers.append(Oper(cfg.next_oper_id(), OperType.BEGIN))
-    node1.Opers.append(Oper(cfg.next_oper_id(), OperType.END))
-    #node1.Opers = [' 1. BEGIN', ' 2. P0 = a > b', ' 3. JUMP P0==T', ' 4. JUMP P0=F', ' 5. END']
+    n1 = g.new_node()
+    n1.Opers = [' 1. BEGIN', ' 2. P0 = a > b', ' 3. JUMP P0==T', ' 4. JUMP P0=F', ' 5. END']
 
     # Node 2.
-    node2 = cfg.new_node()
-    node1.Opers.append(Oper(cfg.next_oper_id(), OperType.BEGIN))
-    node1.Opers.append(Oper(cfg.next_oper_id(), OperType.END))
-    #node2.Opers = [' 6. BEGIN', ' 7. V0 = a + b', ' 8. R = V0', ' 9. END']
+    n2 = g.new_node()
+    n2.Opers = [' 6. BEGIN', ' 7. V0 = a + b', ' 8. R = V0', ' 9. END']
 
     # Node 3.
-    node3 = cfg.new_node()
-    node1.Opers.append(Oper(cfg.next_oper_id(), OperType.BEGIN))
-    node1.Opers.append(Oper(cfg.next_oper_id(), OperType.END))
-    #node3.Opers = ['10. BEGIN', '11. V1 = a - b', '12. R = V1', '13. END']
+    n3 = g.new_node()
+    n3.Opers = ['10. BEGIN', '11. V1 = a - b', '12. R = V1', '13. END']
 
     # Node 4.
-    node4 = cfg.new_node()
-    node4.Opers = []
+    n4 = g.new_node()
+    n4.Opers = []
 
     # Add edges.
-    cfg.add_edge(node0, node1)
-    cfg.add_edge(node1, node2)
-    cfg.add_edge(node1, node3)
-    cfg.add_edge(node2, node4)
-    cfg.add_edge(node3, node4)
+    g.add_edge(n0, n1)
+    g.add_edge(n1, n2)
+    g.add_edge(n1, n3)
+    g.add_edge(n2, n4)
+    g.add_edge(n3, n4)
 
     # Print.
-    cfg.print()
+    g.print()
 
 # ==================================================================================================
 
@@ -62,14 +53,14 @@ def case_001_parser_parse():
     Build case_001 with parser.
     """
 
-    parser = Parser()
+    parser = cfg.Parser()
     parser.parse('cases/001_if.c').print()
 
 # ==================================================================================================
 
 
 if __name__ == '__main__':
-    # case_001_build_manual()
-    case_001_parser_parse()
+    case_001_build_manual()
+    # case_001_parser_parse()
 
 # ==================================================================================================
