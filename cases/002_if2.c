ce4b5c63da017dccf13e11fb762216d24d3c5395
Alexey Rybakov <rybakov.aax@gmail.com>
2022-01-13 21:40:55 +0300

    cases

---
+++ cases/002_if2.c
@@ -0,0 +1,20 @@
+float fun(float a, float b, float &c, float &d)
+{
+    if (a > b)
+    {
+        c = a + b;
+    }
+    else
+    {
+        c = a - b;
+    }
+    
+    if (b < 2 * a)
+    {
+        d = a * b;
+    }
+    else
+    {
+        d = a / b;
+    }
+}
