e17decea48fd5e3bc410be6d052d20071676a5a1
Alexey Rybakov <rybakov.aax@gmail.com>
2022-02-04 16:28:40 +0300

    #13 : run selected cases

--- pyavx512/test.py
+++ pyavx512/test.py
@@ -1,17 +1,57 @@
 """
 Test file.
 """
-import os
 
+import os
 import sem
 import tools
 
+# ==================================================================================================
+
+
+def is_close(expected, actual, k, rel_tol=1e-09, abs_tol=0.1):
+    """
+    Check if expected and actual results are close.
+
+    Parameters
+    ----------
+    expected
+        Expected result.
+    actual
+        Actual result.
+    k
+        Parameter name.
+    rel_tol
+        Relative accuracy.
+    abs_tol
+        Absolute accuracy.
+
+    Returns
+    -------
+        True, if expected and actual results are close,
+        False, otherwise.
+    """
+
+    ok = abs(expected - actual) <= max(rel_tol * max(abs(expected), abs(actual)), abs_tol)
+
+    if not ok:
+        raise AssertionError(f'Param name {k}: got {actual}, expected {expected}.')
 
 # ==================================================================================================
 
-def case_parser_parse(name, input, result):
+
+def run_case(name, input, result):
     """
-    Build case with parser.
+    Run case with results check.
+
+    Parameters
+    ----------
+    name
+        Case name.
+    input
+        Input data.
+    result
+        Correct results.
     """
 
     parser = sem.Parser()
@@ -23,20 +63,21 @@ def case_parser_parse(name, input, result):
 
     for k, v in result.items():
         for i in range(len(v)):
-            isclose(result[k][i], data[k][i], k)
-
-
-# ==================================================================================================
-
-def isclose(expected, actual, k, rel_tol=1e-09, abs_tol=0.1):
-    ok = abs(expected - actual) <= max(rel_tol * max(abs(expected), abs(actual)), abs_tol)
-    if not ok:
-        raise AssertionError(f'Param name {k}: got {actual}, expected {expected}.')
-
+            is_close(result[k][i], data[k][i], k)
 
 # ==================================================================================================
 
-def case_parser_dump():
+
+def dump_all_cases(names=None):
+    """
+    Dump all cases from folder "cases".
+
+    Parameters
+    ----------
+    names
+        Names of cases.
+    """
+
     input_path = 'cases'
     output_path = 'out'
 
@@ -46,6 +87,9 @@ def case_parser_dump():
         os.makedirs(output_path)
 
     for entry in os.scandir(input_path):
+        if not names is None:
+            if not entry.name.split('.')[0] in names:
+                continue
         if not entry.name.endswith('.c'):
             continue
 
@@ -64,32 +108,11 @@ def case_parser_dump():
         f.write(content)
         f.close()
 
-
 # ==================================================================================================
 
 
 if __name__ == '__main__':
-    # ab = {
-    #     'a': [6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0],
-    #     'b': [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
-    # }
-    #
-    # riemann_guessp = {'dl': [1], 'ul': [2], 'pl': [3], 'cl': [4], 'dr': [5], 'ur': [6], 'pr': [7], 'cr': [8]}
-    # riemann_prefun = {'p': [1], 'dk': [2], 'pk': [3], 'ck': [4]}
-    # riemann_sample = {'dl': [1], 'ul': [2], 'vl': [3], 'wl': [4], 'pl': [5], 'cl': [6], 'dr': [7], 'ur': [8], 'vr': [9],
-    #                   'wr': [10], 'pr': [11], 'cr': [12], 'pm': [13], 'um': [14]}
 
-    # case_parser_parse('001_if.c',ab, {'c': [6.0, 6.0, 6.0, 0.0, -2.0, -4.0, -6.0]})
-    # case_parser_parse('002_if2.c',ab, {'c': [6.0, 6.0, 6.0, 0.0, -2.0, -4.0, -6.0], 'd': [0.0, 5.0, 8.0, 9.0, 0.5, 0.2, 0.0]})
-    # case_parser_parse('003_cnst.c',ab, {'c': [6.5, 6.5, 6.5, 9.0, -2.5, -4.5, -6.5]})
-    # case_parser_parse('004_cnst_fold.c', ab, {'c': [6.0, 6.0, 6.0, 0.7833333333333332, 6.0, 6.0, 6.0]})
-    # case_parser_parse('008_pow_ternary.c', ab, {'c': [1.0, 5.0, 16.0, 27.0, 16.0, 5.0, 1.0]})
-    # case_parser_parse('009_pow_pow_ternary.c', ab, {'c': [1.0, 3125.0, 1.8446744073709552e+19, 4.434264882430378e+38, 1.8446744073709552e+19, 3125.0, 1.0]})
-    # case_parser_parse('010_riemann_guessp.c', riemann_guessp, {'pm': [-94059.6]})
-    # case_parser_parse('011_riemann_prefun.c', riemann_prefun, {'f': [-10.666666], 'fd': [1.125]})
-    # case_parser_parse('013_riemann_sample.c', riemann_sample,
-    #                   {'d': [0.5180722891566264], 'u': [14], 'v': [3], 'w': [4], 'p': [13]})
+    dump_all_cases(['001_if'])
 
-    case_parser_dump()
-
-    # ==================================================================================================
+# ==================================================================================================
